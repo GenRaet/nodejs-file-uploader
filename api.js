2058e38f58f1bf6cefbb5ece9209de6b6f8498bf
Ogikusa <ikw.haruto1204@gmail.com>
2022-09-22 17:52:33 +0900

    リファクタリング

---
+++ api.js
@@ -0,0 +1,47 @@
+import Express from "express";
+import FormData from "express-form-data";
+import path from "path";
+import sharp from "sharp";
+
+const rootPath = path.dirname(process.argv[1]);
+
+const router = Express.Router();
+
+router.use(FormData.parse({ uploadDir: path.join(rootPath, "tmp"), autoClean: true }));
+
+router.post("/api/upload", (req, res) => {
+    console.log(`リクエストを受信しました リモートアドレス ${req.ip}`);
+    const n = req.files.image.length;
+    for (let i = 0; i < n; i++) {
+        let src = req.files.image[i].path;
+        let name = req.files.image[i].name;
+        sharp(src)
+            .webp({ quality: 100 })
+            .toFile(fileDir + "/" + path.basename(name) + ".webp");
+        console.log(`The file have been uploaded. File Name: ${name}`);
+    }
+    res.redirect("/");
+})
+
+router.get("/api/all", (req, res) => {
+    const all = fs.readdirSync(path.join(rootPath, "files"));
+    res.json(all);
+})
+
+router.get("/api/reader", (req, res) => {
+    if (!req.query["filename"])
+        return;
+    const fileStream = fs.createReadStream(path.join(rootPath, "files", req.query["filename"]));
+    fileStream.pipe(res);
+})
+
+router.get("/api/clear", (req, res) => {
+    const all = fs.readdirSync(path.join(rootPath, "files"));
+    all.forEach(v => fs.unlink(path.join(rootPath, "/files/", v), (err) => { throw err; }));
+})
+
+router.get("/api/convert-to-webp", (req, res) => {
+
+})
+
+export default router;
--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const express = require("express")
-const app = express();
-const FormData = require("express-form-data");
-const fs = require("fs");
-const path = require("path");
-const sharp = require('sharp');
+import Express from "express";
+const app = Express();
+import fs from "fs";
+import path from "path";
+
+import apiRoute from "./api.js";
+
+const rootPath = path.dirname(process.argv[1]);
 
 const port = 4848;
-const fileDir = __dirname + "/files";
-const tmpDir = __dirname + "/tmp";
+const fileDir = path.join(rootPath, "files");
+const tmpDir = path.join(rootPath, "tmp");
 
 if (!fs.existsSync(fileDir))
     fs.mkdirSync(fileDir);
@@ -15,44 +17,19 @@ if (!fs.existsSync(fileDir))
 if (!fs.existsSync(tmpDir))
     fs.mkdirSync(tmpDir);
 
-var server = app.listen(port, function () {
+const server = app.listen(port, function () {
     console.info(`ポート番号${port}でホストされています`);
 });
 
-app.use(FormData.parse({ uploadDir: __dirname + "/tmp", autoClean: true }));
-app.use(express.static("public"))
+app.use(Express.static("public"))
 
-app.post("/api/upload", (req, res) => {
-    console.log(`リクエストを受信しました リモートアドレス ${req.ip}`);
-    const n = req.files.image.length;
-    for (let i = 0; i < n; i++) {
-        let src = req.files.image[i].path;
-        let name = req.files.image[i].name;
-        sharp(src)
-            .webp({ quality: 100 })
-            .toFile(fileDir + "/" + path.basename(name) + ".webp");
-        console.log(`The file have been uploaded. File Name: ${name}`);
-    }
-    res.redirect("/");
+app.get("/view", (req, res) => {
+    res.sendFile(path.join(rootPath, "public", "view.html"));
 })
 
-app.get("/api/all", (req, res) => {
-    const all = fs.readdirSync(path.join(path.dirname(process.argv[1]), "files"));
-    res.json(all);
-})
+app.use("/api", apiRoute)
 
-app.get("/api/reader/:filename", (req, res) => {
-    if (!req.params["filename"])
-        return;
-    const fileStream = fs.createReadStream(path.join(path.dirname(process.argv[1]), "files", req.params["filename"]));
-    fileStream.pipe(res);
-})
-
-app.get("/api/clear", (req, res) => {
-    const all = fs.readdirSync(path.join(path.dirname(process.argv[1]), "files"));
-    all.forEach(v => console.log(path.join(process.argv[1]), "/files/", v));
-})
-
-app.get("/api/convert-to-webp", (req, res) => {
-
+server.addListener("error", (err) => {
+    console.error(err);
+    throw err;
 })
